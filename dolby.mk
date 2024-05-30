c14682d0aa9cc68f227e45ededa37ba48f9565a7
arnodorian-r <geekroy98@gmail.com>
2024-05-31 00:16:35 +0530

    dolby: fixup

--- dolby.mk
+++ dolby.mk
@@ -4,14 +4,21 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+LOCAL_PATH := vendor/xiaomi/dolby
+
 # Dolby
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/dolby/dax-default.xml:$(TARGET_COPY_OUT_ODM)/etc/dolby/dax-default.xml \
-    $(LOCAL_PATH)/audio/audio_effects.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio/sku_taro/audio_effects.xml
+    $(LOCAL_PATH)/configs/dolby/dax-default.xml:$(TARGET_COPY_OUT_VENDOR)/etc/dolby/dax-default.xml \
+    $(LOCAL_PATH)/audio/audio_effects.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio/sku_taro/audio_effects.xml \
+    $(LOCAL_PATH)/audio/audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio/sku_taro/audio_policy_configuration.xml \
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/media/media_codecs_vendor_audio.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_vendor_audio.xml \
     $(LOCAL_PATH)/media/media_codecs_dolby_audio.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_dolby_audio.xml
+
+ 
+PRODUCT_COPY_FILES += \
+    prebuilts/vndk/v33/arm64/arch-arm64-armv8-a/shared/vndk-core/libstagefright_foundation.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libstagefright_foundation.so
 
 PRODUCT_PACKAGES += \
     XiaomiDolby
@@ -25,10 +32,11 @@ PRODUCT_VENDOR_PROPERTIES += \
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
     $(LOCAL_PATH)/vintf/framework_compatibility_matrix_dolby.xml
     
-DEVICE_MANIFEST_FILE := \
+DEVICE_MANIFEST_TARO_FILES := \
     $(LOCAL_PATH)/vintf/manifest_dolby.xml
 
 # Sepolicy
-BOARD_VENDOR_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/dolby
+BOARD_VENDOR_SEPOLICY_DIRS += \
+    vendor/xiaomi/dolby/sepolicy/dolby
 
 $(call inherit-product, vendor/xiaomi/dolby/dolby-vendor.mk)
