8f0fafe21ee65a89811a30decc4a1544f93038d3
arnodorian-r <geekroy98@gmail.com>
2024-06-05 18:32:53 +0530

    dolby: ship 32-bit libstagefright_foundation as well

--- dolby.mk
+++ dolby.mk
@@ -18,6 +18,7 @@ PRODUCT_COPY_FILES += \
 
  
 PRODUCT_COPY_FILES += \
+    prebuilts/vndk/v33/arm64/arch-arm-armv8-a/shared/vndk-core/libstagefright_foundation.so:$(TARGET_COPY_OUT_VENDOR)/lib/libstagefright_foundation.so \
     prebuilts/vndk/v33/arm64/arch-arm64-armv8-a/shared/vndk-core/libstagefright_foundation.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libstagefright_foundation.so
 
 PRODUCT_PACKAGES += \
